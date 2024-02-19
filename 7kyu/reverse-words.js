3967558e09906424c233e5ac1e712c1641b06d92
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-02-19 13:36:10 +0100

    reverse words version 2 short version

--- 7kyu/reverse-words.js
+++ 7kyu/reverse-words.js
@@ -18,4 +18,6 @@ function reverseWords(str) {
 }
 
 
-reverseWords('The quick brown fox jumps over the lazy dog.')
+function reverseWords(str) {
+    return str.split(" ").map(word => word.split("").reverse().join("")).join(" ");
+}
