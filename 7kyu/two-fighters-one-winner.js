4a1d6806778309d1aa60bf149269afb19867f431
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-02-20 23:51:17 +0100

    started new challenge

---
+++ 7kyu/two-fighters-one-winner.js
@@ -0,0 +1,26 @@
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+// Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+
+// Example:
+//   declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+
+//   Lew attacks Harry; Harry now has 3 health.
+//   Harry attacks Lew; Lew now has 6 health.
+//   Lew attacks Harry; Harry now has 1 health.
+//   Harry attacks Lew; Lew now has 2 health.
+//   Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+
+
+class Fighter {
+    constructor(name, health, damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+        this.toString = function () { return this.name; };
+    }
+}
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    return "Write your code here";
+}
